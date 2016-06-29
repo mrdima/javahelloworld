f79d7e3a0e9edf604fa61236744edb2fe62ffa45
Mr Dima <dimitri.ars@gmail.com>
2016-06-29 11:51:36 +0000

    change beautiful to pangit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main (String [] args) {
-        System.out.println("Hello beautiful World");
+        System.out.println("Hello pangit World");
     }
 }
